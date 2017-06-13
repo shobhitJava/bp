e2a3cdf0c575e44bffd334056bd028852f27b3e4
shobhitJava <simplyshubh@gmail.com>
2017-06-13 11:50:15 +0530

    added new payload log

--- ufatool.go
+++ ufatool.go
@@ -322,7 +322,7 @@ func createUFA(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) 
 	valMsg := validateNewUFA(who, payload)
 	if valMsg == "" {
 		stub.PutState(ufanumber, []byte(payload))
-		fmt.Println("Payload is "+payload)
+		fmt.Println("new Payload is "+payload)
 		updateMasterRecords(stub, ufanumber)
 		appendUFATransactionHistory(stub, ufanumber, payload)
 		logger.Info("Created the UFA after successful validation : " + payload)
